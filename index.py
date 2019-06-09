a1d6ff5afae2299af8c3b4a7e9e12243fe474c90
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2019-06-09 12:11:42 +0530

    added readme.md and added comments

--- index.py
+++ index.py
@@ -5,21 +5,25 @@ import numpy as np
 
 print("Get read to become invisible")
 
+# allowing camer to boot up
 cap = cv2.VideoCapture(0)
 time.sleep(3)
 background = 0
 for i in range(30):
     ret, background = cap.read()
 
-background = np.flip(background, axis = 1)
+background = np.flip(background, axis=1)
 
 while(cap.isOpened()):
     ret, img = cap.read()
 
-    img = np.flip(img, axis = 1)
+    img = np.flip(img, axis=1)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     value = (35, 35)
     blurred = cv2.GaussianBlur(hsv, value, 0)
+
+    # locating red pixel from current frame using different
+    # shades/range of red color
     lower_red = np.array([0, 120, 70])
     upper_red = np.array([10, 255, 255])
     mask1 = cv2.inRange(hsv, lower_red, upper_red)
@@ -29,9 +33,11 @@ while(cap.isOpened()):
     mask2 = cv2.inRange(hsv, lower_red, upper_red)
 
     mask = mask1 + mask2
+    # masking the detected color pixed with originally captured image
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
     img[np.where(mask == 255)] = background[np.where(mask == 255)]
+    # display image
     cv2.imshow('Display', img)
 
     # esc to exit exectution
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Clock of Invisibility
+
+Run index file and see the magic. 
+
+Note:- Image mapping is done based on red color pixel replacement. So use any material of color red.
